4a8dc9447fca3952d32c85de75e0ad658bd95012
Mario Rodriguez <mario.rodriguez.2006@protonmail.com>
2024-12-10 20:01:05 +0100

    cambio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 import { Home } from './components/home/home';
-import { prueba } from './components/forms/prueba';
 
 function App() {
   return (
